eab15124d2a4708423ae1a2ef7c27b7200fdc78d
berkleylim <berkleylim16@gmail.com>
2023-12-07 15:27:40 +0900

    chron: kotlin Spring Boot 셋팅

---
+++ ver02/src/main/kotlin/com/example/ver02/Ver02Application.kt
@@ -0,0 +1,11 @@
+package com.example.ver02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ver02Application
+
+fun main(args: Array<String>) {
+	runApplication<Ver02Application>(*args)
+}
---
+++ ver02/src/test/kotlin/com/example/ver02/Ver02ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ver02
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ver02ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
